04f170e736deb1d5eaa2f155a5223c1cddb5258e
Amos <musaamosjoe@gmail.com>
2022-07-18 19:20:22 -0700

    Modified 0-safe_print_list.py script

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    no = 0
-    for i in range(0, x):
+    if my_list is None or my_list == [] or x == 0:
+        print("")
+        return 0
+
+    num = 0
+    while x > 0:
         try:
             print("{:d}".format(my_list[i]), end="")
-            no += 1
-        except:
-            pass
-    print()
-    return no
+            num += 1
+        except IndexError:
+            break
+        x -= 1
+
+    print("")
+    return num
